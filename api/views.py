0f747cb9d647a4b1f0453fa035a47509daf7030a
Anderson Cafurica <129119434+Emicy963@users.noreply.github.com>
2025-02-26 15:51:01 +0100

    Creata a Manual Serializer

--- api/views.py
+++ api/views.py
@@ -3,6 +3,8 @@ from django.http import JsonResponse
 from student.models import Student
 
 def students_view(request):
+    """ Thats function call the Students atributes and serializers him manually """
+    """ Serializer is take the complex data and converter or simplify for a simple data for 'JSON' or 'XML'."""
     students = Student.objects.all()
     students_list = list(students.values())
     return JsonResponse(students_list, safe=False)
