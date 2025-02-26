4abd3c97831b9d1fb57519875f0c2c2bed44e240
Anderson Cafurica <129119434+Emicy963@users.noreply.github.com>
2025-02-26 15:08:56 +0100

    Create a Simple API endpoints!

--- api/urls.py
+++ api/urls.py
@@ -2,5 +2,5 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('students/', views.students)
+    path('students/', views.students_view)
 ]
--- api/views.py
+++ api/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
+from django.http import JsonResponse
 
-# Create your views here.
+def students_view(request):
+    students = {
+        'id': 1,
+        'name': 'Anderson Cafurica',
+        'class': 'Computer Science',
+    }
+    return JsonResponse(students)
