39784e9481c93dc570e4cb8730f608717e53ab9d
Anderson Cafurica <129119434+Emicy963@users.noreply.github.com>
2025-02-27 18:19:56 +0100

    Employed Serializer

--- api/serializers.py
+++ api/serializers.py
@@ -1,7 +1,13 @@
 from rest_framework import serializers
 from student.models import Student
+from employed.models import Employed
 
 class StudentSerializer(serializers.ModelSerializer):
     class Meta:
         model = Student
         fields = "__all__"
+
+class EmployedSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Employed
+        fields = '__all__'
