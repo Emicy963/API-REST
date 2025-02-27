2dcfc0e9fd2de8b57573c0c2cb647bf3b76500ee
Anderson Cafurica <129119434+Emicy963@users.noreply.github.com>
2025-02-27 18:02:23 +0100

    Delete Operation

--- api/views.py
+++ api/views.py
@@ -17,10 +17,9 @@ def students_view(request):
         if serializer.is_valid():
             serializer.save()
             return Response(serializer.data, status=status.HTTP_201_CREATED)
-        print(serializer.errors)
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
     
-@api_view(['GET', 'PUT'])
+@api_view(['GET', 'PUT', 'DELETE'])
 def student_detail_view(request, pk):
     try:
         student = Student.objects.get(pk=pk)
@@ -38,3 +37,7 @@ def student_detail_view(request, pk):
             return Response(serializer.data, status=status.HTTP_200_OK)
         else:
             return Response(status=status.HTTP_400_BAD_REQUEST)
+        
+    elif request.method == 'DELETE':
+        student.delete()
+        return Response(status=status.HTTP_204_NO_CONTENT)
