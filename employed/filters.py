a3d24741b4c75d69d670c79e65b5707d509669ab
Anderson Cafurica <129119434+Emicy963@users.noreply.github.com>
2025-03-01 22:00:05 +0100

    Filtiring with name and id

--- employed/filters.py
+++ employed/filters.py
@@ -3,7 +3,10 @@ from .models import Employed
 
 class EmployeeFilter(django_filters.FilterSet):
     designation = django_filters.CharFilter(field_name='designation', lookup_expr='iexact')
+    emp_name = django_filters.CharFilter(field_name='emp_name', lookup_expr='icontains')
+    id = django_filters.RangeFilter(field_name='id')
+
 
     class Meta:
         model = Employed
-        fields = ['designation']
+        fields = ['designation', 'emp_name', 'id']
